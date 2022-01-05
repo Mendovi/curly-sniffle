b70c2ff8f59470c2c953c7661bc7a82760c50125
Mendovi <96690983+Mendovi@users.noreply.github.com>
2022-01-05 23:33:40 +0700

    Update wow.js

--- wow.js
+++ wow.js
@@ -5,28 +5,28 @@ function car_down () {x = 0;
 x = document.getElementById("moto").offsetTop;
 xn = x + 50;
 document.getElementById("moto").style.top = xn+"px"
-socket.send(xn);
+socket.send(document.getElementById("moto").style.left);
 }
 function car_up () {
 x = 0;
 x = document.getElementById("moto").offsetTop;
 xn = x - 50;
 document.getElementById("moto").style.top = xn+"px"
-socket.send(xn);
+socket.send(document.getElementById("moto").style.left);
 }
 function car_left () {
 x = 0;
 x = document.getElementById("moto").offsetLeft;
 xn = x - 50;
 document.getElementById("moto").style.left = xn+"px"
-socket.send(xn);
+socket.send(document.getElementById("moto").style.left);
 }
 function car_right () {
 x = 0;
 x = document.getElementById("moto").offsetLeft;
 xn = x + 50;
 document.getElementById("moto").style.left = xn+"px"
-socket.send(xn);
+socket.send(document.getElementById("moto").style.left);
 }
 
 
@@ -34,28 +34,28 @@ function cat_down () {x = 0;
 x = document.getElementById("caat").offsetTop;
 xn = x + 50;
 document.getElementById("caat").style.top = xn+"px"
-socket.send(xn);
+socket.send(document.getElementById("caat").style.top);
 }
 function cat_up () {
 x = 0;
 x = document.getElementById("caat").offsetTop;
 xn = x - 50;
 document.getElementById("caat").style.top = xn+"px"
-socket.send(xn);
+socket.send(document.getElementById("caat").style.top);
 }
 function cat_left () {
 x = 0;
 x = document.getElementById("caat").offsetLeft;
 xn = x - 50;
 document.getElementById("caat").style.left = xn+"px"
-socket.send(xn);
+socket.send(document.getElementById("caat").style.top);
 }
 function cat_right () {
 x = 0;
 x = document.getElementById("caat").offsetLeft;
 xn = x + 50;
 document.getElementById("caat").style.left = xn+"px"
-socket.send(xn);
+socket.send(document.getElementById("caat").style.top);
 }
 	
 
