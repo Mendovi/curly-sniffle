1452b6fc500b190d06084e36dd1484220956b273
Mendovi <96690983+Mendovi@users.noreply.github.com>
2022-01-05 21:11:08 +0700

    Update wow.js

--- wow.js
+++ wow.js
@@ -1,36 +1,34 @@
+let socket = new WebSocket("wss://mendovi.github.io/curly-sniffle/");
+
 
 function car_down () {x = 0;
 x = document.getElementById("moto").offsetTop;
 xn = x + 50;
 document.getElementById("moto").style.top = xn+"px"
+socket.send(xn);
 }
 function car_up () {
 x = 0;
 x = document.getElementById("moto").offsetTop;
 xn = x - 50;
 document.getElementById("moto").style.top = xn+"px"
+socket.send(xn);
 }
 function car_left () {
 x = 0;
 x = document.getElementById("moto").offsetLeft;
 xn = x - 50;
 document.getElementById("moto").style.left = xn+"px"
+socket.send(xn);
 }
 function car_right () {
 x = 0;
 x = document.getElementById("moto").offsetLeft;
 xn = x + 50;
 document.getElementById("moto").style.left = xn+"px"
+socket.send(xn);
 }
-
-
-let socket = new WebSocket("wss://mendovi.github.io/");
-
-socket.onopen = function(e) {
-  alert("Отправляем данные на сервер");
-  socket.send(position);
-};
-		
+	
 
 
 
