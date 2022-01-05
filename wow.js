858ba13b80b734e5939658af5ad9a11f0d4449d8
Mendovi <96690983+Mendovi@users.noreply.github.com>
2022-01-05 22:25:49 +0700

    Update wow.js

--- wow.js
+++ wow.js
@@ -28,6 +28,35 @@ xn = x + 50;
 document.getElementById("moto").style.left = xn+"px"
 socket.send(xn);
 }
+
+
+function cat_down () {x = 0;
+x = document.getElementById("caat").offsetTop;
+xn = x + 50;
+document.getElementById("caat").style.top = xn+"px"
+socket.send(xn);
+}
+function cat_up () {
+x = 0;
+x = document.getElementById("caat").offsetTop;
+xn = x - 50;
+document.getElementById("caat").style.top = xn+"px"
+socket.send(xn);
+}
+function cat_left () {
+x = 0;
+x = document.getElementById("caat").offsetLeft;
+xn = x - 50;
+document.getElementById("caat").style.left = xn+"px"
+socket.send(xn);
+}
+function cat_right () {
+x = 0;
+x = document.getElementById("caat").offsetLeft;
+xn = x + 50;
+document.getElementById("caat").style.left = xn+"px"
+socket.send(xn);
+}
 	
 
 
