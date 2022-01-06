419ac4f14f94ebb0fa1cb3779b680957eca71424
Mendovi <96690983+Mendovi@users.noreply.github.com>
2022-01-06 18:45:28 +0700

    Update wow.js

--- wow.js
+++ wow.js
@@ -1,9 +1,6 @@
 let socket = new WebSocket("wss://mendovi.github.io/curly-sniffle/");
 
 
-
-
-
 function car_down () {x = 0;
 x = document.getElementById("moto").offsetTop;
 xn = x + 50;
@@ -31,8 +28,6 @@ xn = x + 50;
 document.getElementById("moto").style.left = xn+"px"
 socket.send(document.getElementById("moto").style.left);
 }
-
-
 function cat_down () {x = 0;
 x = document.getElementById("caat").offsetTop;
 xn = x + 50;
@@ -60,29 +55,3 @@ xn = x + 50;
 document.getElementById("caat").style.left = xn+"px"
 socket.send(document.getElementById("caat").style.top);
 }
-	
-
-
-
-/*function countFunc(count) {
-  var btnPlus = count.querySelector('.js-plus-btn');
-  var btnMinus = count.querySelector('.js-minus-btn');
-  var field = count.querySelector('.js-number');
-  var fieldValue = parseFloat(field.value, 10);
-
-  btnMinus.addEventListener('click', function() {
-    if (fieldValue > 1) {
-      fieldValue--;
-      field.value = fieldValue;
-    } else {
-      return 1;
-    }
-  });
-  btnPlus.addEventListener('click', function() {
-    fieldValue++;
-    field.value = fieldValue;
-  });
-
-}
-var counts = document.querySelectorAll('.wrapper');
-counts.forEach(countFunc);
